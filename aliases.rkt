21ecfb218a0fdc2b8635ca51ec868957218c7027
Vityou <zlee314@gmail.com>
2018-07-13 16:29:58 -0600

    check if terminal is an xterm

--- aliases.rkt
+++ aliases.rkt
@@ -16,10 +16,12 @@
 
 
 (define best-ls-command
-  (cond
-    [(find-executable-path "ls_extended") 'ls_extended]
-    [(find-executable-path "exa") 'exa]
-    [else "ls --color=auto"]))
+  (if (string=? (sub-string (getenv "TERM") 0 5) "xterm")
+      (cond
+        [(find-executable-path "ls_extended") 'ls_extended]
+        [(find-executable-path "exa") 'exa]
+        [else "ls --color=auto"])
+      "ls --color=auto"))
 
 
 ; ls aliases
