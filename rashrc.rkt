2e6c0ef029da49ba1ebbb4e03a807fd937ad67dc
Vityou <zlee314@gmail.com>
2018-07-14 21:36:02 -0600

    add require macros

--- rashrc.rkt
+++ rashrc.rkt
@@ -1,6 +1,7 @@
 #lang rash
 
-(require reprovide/reprovide) ; requires and provides all defined out
+(require reprovide/reprovide ; requires and provides all defined out
+         racket/require-syntax)
 
 (provide (all-defined-out))
 
@@ -10,3 +11,17 @@
            (for-syntax racket/base syntax/parse)
            (only-in rash/demo/setup val))
 
+
+(define-require-syntax (here stx)
+  (syntax-parse stx
+    [(_ path:str)
+     (datum->syntax stx `(file ,(string-append (path->string (current-directory))
+                                               (syntax->datum #'path))))]))
+
+(define-syntax (req-here stx)
+  (syntax-parse stx
+    [(_ path:str)
+     (datum->syntax stx `(require
+                          (file ,(string-append
+                                  (path->string (current-directory))
+                                  (syntax->datum #'path)))))]))
