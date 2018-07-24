c557b694b56fd7826544acff665ba7673714dd74
Vityou <zlee314@gmail.com>
2018-07-23 21:14:44 -0600

    remove here syntax as it works normally now

--- rashrc.rkt
+++ rashrc.rkt
@@ -1,7 +1,6 @@
 #lang rash
 
-(require reprovide/reprovide ; requires and provides all defined out
-         racket/require-syntax)
+(require reprovide/reprovide) ; requires and provides all defined out
 
 (provide (all-defined-out))
 
@@ -10,18 +9,3 @@
            "line-macros.rkt"
            (for-syntax racket/base syntax/parse)
            (only-in rash/demo/setup val))
-
-
-(define-require-syntax (here stx)
-  (syntax-parse stx
-    [(_ path:str)
-     (datum->syntax stx `(file ,(string-append (path->string (current-directory))
-                                               (syntax->datum #'path))))]))
-
-(define-syntax (req-here stx)
-  (syntax-parse stx
-    [(_ path:str)
-     (datum->syntax stx `(require
-                          (file ,(string-append
-                                  (path->string (current-directory))
-                                  (syntax->datum #'path)))))]))
