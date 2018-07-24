eb357d93d41b53d00be5e17f64aaf3f2542ab79e
Vityou <zlee314@gmail.com>
2018-07-23 21:24:42 -0600

    improved exit macro

--- line-macros.rkt
+++ line-macros.rkt
@@ -54,7 +54,10 @@ slm ../.. = cd ../..
 
 
 ; this file prevents the repl from closing
-slm exit-repl = (begin (when (file-exists? (string-append (getenv "HOME") ".racket/.LOCKracket-prefs.rktd"))
-                             (delete-file (string-append (getenv "HOME") ".racket/.LOCKracket-prefs.rktd")))
+slm exit-repl = (begin (when (and (getenv "HOME")
+                                  (file-exists? (string-append (getenv "HOME") 
+                                                               ".racket/.LOCKracket-prefs.rktd"))
+                             (delete-file (string-append (getenv "HOME") 
+                                                         ".racket/.LOCKracket-prefs.rktd")))
                        (exit))
 
